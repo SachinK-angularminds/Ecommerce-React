a84d05079fd2d3f570874b55bb37a29e18747402
Prathameshs28 <prathameshs2801@gmail.com>
2025-03-19 18:29:30 +0530

    solved react-routing configuration in jest

--- src/babel.config.js
+++ src/babel.config.js
@@ -1,8 +1,11 @@
 
-export default {
+module.exports = {
   presets: [
     '@babel/preset-env',
     '@babel/preset-react',
-    
   ],
+  plugins: [
+    '@babel/plugin-transform-modules-commonjs', // Make sure to support CommonJS transformation
+  ]
 };
+
--- src/jest.config.js
+++ src/jest.config.js
@@ -3,6 +3,14 @@ module.exports = {
     transform: {
       '^.+\\.jsx?$': 'babel-jest', // Ensure babel-jest is transforming JSX files
     },
+    transformIgnorePatterns: [
+      // Ignore transformation for non-JS files in node_modules (e.g., assets, styles)
+      '/node_modules/(?!axios|other-package-to-transform)/',  // Keep only `axios` (or any other specific package) to be transformed
+  
+      // Regex to ignore public assets like images, fonts, etc.
+      '/node_modules/.*\\.(css|less|scss|png|jpg|gif|svg)$/', // This line excludes assets from transformation
+    ],
+  
     moduleNameMapper: {
       '\\.css$': 'identity-obj-proxy', // Mock CSS imports
       '^react-router-dom$': require.resolve('react-router-dom'), // Resolving react-router-dom
